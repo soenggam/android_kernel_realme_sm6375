bb7206bc87e7b5072c1185e208cb63eea2d84724
ShivamKumarJha <jha.shivam3@gmail.com>
2022-07-26 01:15:01 -0400

    Add fs/ modifications
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -73,6 +73,7 @@
 
 #include <trace/events/sched.h>
 
+
 int suid_dumpable = 0;
 
 static LIST_HEAD(formats);
@@ -1724,7 +1725,9 @@ static int exec_binprm(struct linux_binprm *bprm)
 
 	return ret;
 }
-
+#ifdef CONFIG_OPLUS_KERNEL_SECURE_GUARD
+extern int oplus_exec_block(struct file *file);
+#endif /* CONFIG_OPLUS_KERNEL_SECURE_GUARD */
 /*
  * sys_execve() executes a new program.
  */
@@ -1778,7 +1781,13 @@ static int __do_execve_file(int fd, struct filename *filename,
 	retval = PTR_ERR(file);
 	if (IS_ERR(file))
 		goto out_unmark;
-
+#ifdef CONFIG_OPLUS_KERNEL_SECURE_GUARD
+    retval = oplus_exec_block(file);
+	if (retval){
+		fput(file);
+		goto out_unmark;
+	}
+#endif /* CONFIG_OPLUS_KERNEL_SECURE_GUARD */
 	sched_exec();
 
 	bprm->file = file;
